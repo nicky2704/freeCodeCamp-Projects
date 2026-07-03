6bba44e416c9cd87303f303b34d3f6ca153ff8f8
nicky2704 <nikkikumari1192@gmail.com>
2026-07-03 22:40:33 +0530

    Initial commit

---
+++ JavaScript/sentence-marker.js
@@ -0,0 +1,26 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "red";
+noun = "dragon";
+noun2 = "animals";
+place = "cave";
+adjective2 = "huge";
+verb = "flying"
+
+const firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+"."
+console.log("First story: "+ firstStory);
+
+adjective = "green";
+noun = "dinosaur";
+noun2 = "organisms";
+place = "jungle";
+adjective2 = "big";
+verb = "shouting"
+
+const secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+ ". The "+noun+" lived in a "+place+" and had "+ adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+secondStory);
